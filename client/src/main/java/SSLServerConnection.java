b48bcd445fa673c295632dd9050ce3c2e18d9ed0
alexw0610 <alex@mailbag.at>
2021-08-30 21:34:25 +0200

    [SCM] added config file retrieval and jar file retrieval to ApplicationProperties

--- client/src/main/java/SSLServerConnection.java
+++ client/src/main/java/SSLServerConnection.java
@@ -7,7 +7,6 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -107,7 +106,7 @@ public class SSLServerConnection {
             keyStore.load(null, applicationProperties.getProperty("trustStorePassword").toCharArray());
             CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
             String certificateFileName = applicationProperties.getProperty("serverCertificateName");
-            Certificate certificate = certificateFactory.generateCertificate(new FileInputStream(applicationProperties.getFileFromResourcePath(certificateFileName)));
+            Certificate certificate = certificateFactory.generateCertificate(applicationProperties.getResourceAsStream(certificateFileName));
             keyStore.setCertificateEntry("openglDungeonCertificate", certificate);
             trustManagerFactory.init(keyStore);
             sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
--- common/src/main/java/util/ApplicationProperties.java
+++ common/src/main/java/util/ApplicationProperties.java
@@ -1,32 +1,46 @@
 package util;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.CodeSource;
 import java.util.Properties;
 
 public class ApplicationProperties extends Properties {
 
     private static final String APPLICATION_PROPERTIES_FILE = "application.properties";
+    private static final long serialVersionUID = 2994373857836726368L;
+    private static String basePath;
+    public static final String CONFIG_PATH = "/config/";
 
 
     public ApplicationProperties() {
         super();
         try {
-            this.load(new FileInputStream(getFileFromResourcePath(APPLICATION_PROPERTIES_FILE)));
+            this.load(getResourceAsStream(APPLICATION_PROPERTIES_FILE));
         } catch (IOException e) {
             System.err.println(String.format("Error loading properties file: %s", APPLICATION_PROPERTIES_FILE));
             System.exit(1);
         }
-    }
-
-    public File getFileFromResourcePath(String filename) {
-        URL requestedURL = Thread.currentThread().getContextClassLoader().getResource(filename);
-        if (requestedURL == null) {
-            System.err.println(String.format("Error file not found: %s", filename));
+        CodeSource src = ApplicationProperties.class.getProtectionDomain().getCodeSource();
+        URL jar = src.getLocation();
+        File parent = null;
+        try {
+            parent = new File(jar.toURI());
+            basePath = parent.getParent();
+        } catch (URISyntaxException e) {
+            System.err.println(String.format("Error while setting base path: %s", jar.getPath()));
             System.exit(1);
         }
-        return new File(Thread.currentThread().getContextClassLoader().getResource(filename).getFile());
+    }
+
+    public File getFileFromConfig(String filename) {
+        return new File(basePath + CONFIG_PATH + filename);
+    }
+
+    public InputStream getResourceAsStream(String filename) {
+        return this.getClass().getResourceAsStream("/" + filename);
     }
 }
