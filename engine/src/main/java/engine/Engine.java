cf5386d8905de73a455a44aa9969bf0bbfd3db4b
alexw0610 <alex@mailbag.at>
2021-10-01 13:37:33 +0200

    [SCM] reworked Engine and Display class

--- engine/src/main/java/engine/Engine.java
+++ engine/src/main/java/engine/Engine.java
@@ -6,20 +6,53 @@ import com.jogamp.newt.opengl.GLWindow;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.util.FPSAnimator;
+import engine.component.RenderComponent;
+import engine.component.TransformationComponent;
+import engine.entity.Entity;
+import engine.entity.EntityBuilder;
+import engine.enums.PrimitiveMeshShape;
+import engine.enums.ShaderType;
+import engine.enums.TextureKey;
+import engine.object.Mesh;
+import engine.system.RenderSystem;
+import engine.system.System;
+import engine.system.TransformationSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static engine.EngineConstants.*;
 
 public class Engine {
+    private final List<Entity> entities = new ArrayList<>();
+    private final List<System> systems = new ArrayList<>();
+
     public void start() {
         setupDisplay();
     }
 
-    private static void setupDisplay() {
+    public void init() {
+        systems.add(new RenderSystem());
+        systems.add(new TransformationSystem());
+        entities.add(EntityBuilder.builder()
+                .withComponent(new RenderComponent(new Mesh(PrimitiveMeshShape.TRIANGLE), TextureKey.DEFAULT, ShaderType.DEFAULT))
+                .withComponent(new TransformationComponent())
+                .build());
+        entities.add(EntityBuilder.builder()
+                .withComponent(new RenderComponent(new Mesh(PrimitiveMeshShape.TRIANGLE), TextureKey.DEFAULT, ShaderType.DEFAULT))
+                .build());
+        entities.add(EntityBuilder.builder()
+                .withComponent(new TransformationComponent())
+                .build());
+    }
+
+    private void setupDisplay() {
         GLProfile glp = GLProfile.getDefault();
         GLCapabilities caps = new GLCapabilities(glp);
         GLWindow window = GLWindow.create(caps);
 
-        final FPSAnimator animator = new FPSAnimator(window, FPS, true);
+        FPSAnimator animator = new FPSAnimator(window, FPS, true);
 
         window.addWindowListener(new WindowAdapter() {
             @Override
@@ -29,20 +62,30 @@ public class Engine {
                     public void run() {
                         if (animator.isStarted())
                             animator.stop();
-                        System.exit(0);
+                        java.lang.System.exit(0);
                     }
                 }.start();
             }
         });
 
         window.setSize((int) WINDOW_WIDTH, (int) WINDOW_HEIGHT);
-        window.addGLEventListener(new Display());
+        window.addGLEventListener(new Display(this));
         InputListener inputListener = new InputListener();
         window.addKeyListener(inputListener);
         window.addMouseListener(inputListener);
         window.setTitle(TITLE);
-        window.setVisible(true);
         animator.start();
+        window.setVisible(true);
+    }
+
+    public void step() {
+        for (System system : this.systems) {
+            system.processEntities(this.entities.stream().filter(system::isResponsibleFor).collect(Collectors.toList()));
+        }
+        //renderService.renderNextFrame();
+        //keyHandler.processActiveKeys();
+        //physicsService.doPhysics(RenderHandler.RENDER_HANDLER.getCurrentFrameDeltaMs());
+        //Camera.CAMERA.lerpToLookAtTarget(RenderHandler.RENDER_HANDLER.getCurrentFrameDeltaMs());
     }
 
 }
--- engine/src/main/java/engine/EngineConstants.java
+++ engine/src/main/java/engine/EngineConstants.java
@@ -2,7 +2,7 @@ package engine;
 
 public class EngineConstants {
 
-    public static final String TITLE = "Dungeon";
+    public static final String TITLE = "openglDungeon";
     public static final double WINDOW_WIDTH = 1280;
     public static final double WINDOW_HEIGHT = 960;
     public static final int FPS = 60;
